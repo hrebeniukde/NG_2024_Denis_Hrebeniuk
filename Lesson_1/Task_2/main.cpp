234d5041a345d6d85b616067f320392368735ffc
Denys Hrebeniuk <denis.hrebeniuk2006@ukr.net>
2024-06-05 18:27:11 +0300

    Replace single-character variable names in task №2 of lession №1

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -7,13 +7,13 @@ int main()
     cout << "Hello, I am a calculator!" << endl;
     cout << "Please, write two numbers to get their sum." << endl;
 
-    int a, b;
-    cout << "Enter a: ";
-    cin >> a;
-    cout << "Enter b: ";
-    cin >> b;
+    int firstNumber, secondNumber;
+    cout << "Enter first number: ";
+    cin >> firstNumber;
+    cout << "Enter second number: ";
+    cin >> secondNumber;
 
-    cout << "Result: " <<  a << " + " << b << " = " << a + b << endl;
+    cout << "Result: " <<  firstNumber << " + " << secondNumber << " = " << firstNumber + secondNumber << endl;
 
     return 0;
 }
