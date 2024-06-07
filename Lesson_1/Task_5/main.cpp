0cbe7e2a0b208591d32b97573f13092a7e35aff4
Denys Hrebeniuk <denis.hrebeniuk2006@ukr.net>
2024-06-07 12:55:54 +0300

    Add task №5 of lession №1

---
+++ Lesson_1/Task_5/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+
+using namespace std;
+
+void printComplexDoubleNumber(complex<double> number)
+{
+    cout << number.real();
+
+    if (number.imag() > 0) {
+        cout << " + " << number.imag() << "i";
+    } else if (number.imag() < 0) {
+        cout << " - " << -number.imag() << "i";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    cout << "Hi, I can help you solve any quadratic equation!" << endl;
+    cout << "Note: if your quadratic equation is incomplete, simply specify 0 in the missing values" << endl;
+    cout << endl;
+
+    double numberA, numberB, numberC;
+    complex<double> discriminant, firstRoot, secondRoot;
+
+    cout << "Please, enter a: ";
+    cin >> numberA;
+
+    cout << "Please, enter b: ";
+    cin >> numberB;
+
+    cout << "Please, enter c: ";
+    cin >> numberC;
+
+    cout << endl;
+    if (numberA && numberB && numberC) {
+        discriminant = pow(numberB, 2) - 4 * numberA * numberC;
+        firstRoot = (-numberB + sqrt(discriminant)) / (2.0 * numberA);
+        secondRoot = (-numberB - sqrt(discriminant)) / (2.0 * numberA);
+
+        cout << "Discriminant = ";
+        printComplexDoubleNumber(discriminant);
+    } else if (numberA && !numberB && !numberC) {
+        firstRoot = 0;
+        secondRoot = 0;
+    } else if (numberA && numberB && !numberC) {
+        firstRoot = 0;
+        secondRoot = -(numberB / numberA);
+    } else if (numberA && !numberB && numberC) {
+        double root = -(numberC / numberA);
+
+        if (root > 0) {
+            firstRoot = sqrt(root);
+            secondRoot = -sqrt(root);
+        } else {
+            cout << "No further solution to the quadratic equation is possible, the root cannot be negative";
+            return 0;
+        }
+    } else {
+        cout << "Error, you have specified incorrect input data";
+        return 0;
+    }
+
+    cout << "First root = ";
+    printComplexDoubleNumber(firstRoot);
+    cout << "Second root = ";
+    printComplexDoubleNumber(secondRoot);
+
+    return 0;
+}
