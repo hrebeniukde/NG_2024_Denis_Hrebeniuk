fd3cce2baf618a8c619795565ef9737458236a09
Denys Hrebeniuk <denis.hrebeniuk2006@ukr.net>
2024-06-19 14:29:41 +0300

    Improve and simplify code, correct one character names of variables in task №6 of lesson №1

--- Lesson_1/Task_6/main.cpp
+++ Lesson_1/Task_6/main.cpp
@@ -2,31 +2,33 @@
 
 using namespace std;
 
+void fillLine(int amountToSkip, int rowLength) {
+    for (int symbol = 1; symbol <= rowLength; symbol++) {
+        if (symbol > amountToSkip && symbol <= rowLength - amountToSkip) {
+            cout << "*";
+        } else {
+            cout << " ";
+        }
+    }
+}
+
 int main()
 {
     cout << "Let's draw a Christmas tree, shall we?" << endl;
 
     int size;
-
     cout << "Please, enter a size: ";
     cin >> size;
 
     int rowLength = (size * 2) - 1;
 
     cout << endl;
-    for (int i = 1; i <= size + 1; i++) {
-        for (int j = 1; j <= rowLength; j++) {
-            // just calculate how many "cells" we should skip at start and at the end
-            int amountToSkip = (i == size + 1) ? size - 1 : size - i; // if row == 5, just repeat first row iteration behaviour
-
-            if (j > amountToSkip && j <= rowLength - amountToSkip) {
-                cout << "*";
-            } else {
-                cout << " ";
-            }
-        }
+    for (int row = 1; row <= size; row++) {
+        fillLine(size - row, rowLength);
         cout << endl;
     }
+
+    fillLine(size - 1, rowLength);
 
     return 0;
 }
