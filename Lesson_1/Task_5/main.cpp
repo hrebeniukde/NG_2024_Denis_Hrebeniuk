d264457c0618b229d2cd3f2a695035358a7b2776
Denys Hrebeniuk <denis.hrebeniuk2006@ukr.net>
2024-06-20 21:05:29 +0300

    Simplify code logic in task №5 of lesson №1

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -36,34 +36,21 @@ int main()
     cin >> numberC;
 
     cout << endl;
-    if (numberA && numberB && numberC) {
+    if (numberA) {
         discriminant = pow(numberB, 2) - 4 * numberA * numberC;
         firstRoot = (-numberB + sqrt(discriminant)) / (2.0 * numberA);
         secondRoot = (-numberB - sqrt(discriminant)) / (2.0 * numberA);
 
         cout << "Discriminant = ";
         printComplexDoubleNumber(discriminant);
-    } else if (numberA && numberB && !numberC) {
-        secondRoot = -(numberB / numberA);
-    } else if (numberA && !numberB && numberC) {
-        double root = -(numberC / numberA);
-
-        if (root > 0) {
-            firstRoot = sqrt(root);
-            secondRoot = -sqrt(root);
-        } else {
-            cout << "No further solution to the quadratic equation is possible, the root cannot be negative";
-            return 0;
-        }
+        cout << "First root = ";
+        printComplexDoubleNumber(firstRoot);
+        cout << "Second root = ";
+        printComplexDoubleNumber(secondRoot);
     } else {
         cout << "Error, you have specified incorrect input data";
         return 0;
     }
-
-    cout << "First root = ";
-    printComplexDoubleNumber(firstRoot);
-    cout << "Second root = ";
-    printComplexDoubleNumber(secondRoot);
 
     return 0;
 }
