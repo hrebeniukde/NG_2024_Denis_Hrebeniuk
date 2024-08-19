bf600dab010ffc7cd21d43c08670b24f4634a3b6
Denys Hrebeniuk <denis.hrebeniuk2006@ukr.net>
2024-08-19 23:58:23 +0300

    Add .pro file in task №1 of lesson №5 and remove .pro from gitignore file

--- Lesson_5/Task_1/mainwindow.cpp
+++ Lesson_5/Task_1/mainwindow.cpp
@@ -5,37 +5,40 @@
 
 #define MAX_CARS_COUNT 10
 
-EnhancedCar carList[MAX_CARS_COUNT] = {
-    EnhancedCar("Toyota", "Camry", "Sedan", "White", 5, 25000),
-    EnhancedCar("Ford", "Mustang", "Coupe", "Red", 4, 35000),
-    EnhancedCar("Chevrolet", "Tahoe", "SUV", "Black", 7, 50000),
-    EnhancedCar("Honda", "Civic", "Sedan", "Blue", 5, 20000),
-    EnhancedCar("Tesla", "Model S", "Sedan", "Silver", 5, 75000),
-    EnhancedCar("BMW", "X5", "SUV", "Gray", 5, 60000),
-    EnhancedCar("Mercedes-Benz", "C-Class", "Sedan", "Black", 5, 40000),
-    EnhancedCar("Audi", "A4", "Sedan", "White", 5, 37000),
-    EnhancedCar("Jeep", "Wrangler", "SUV", "Green", 4, 45000),
-    EnhancedCar("Porsche", "911", "Coupe", "Yellow", 4, 100000)};
+EnhancedCar carList[MAX_CARS_COUNT]
+    = {EnhancedCar("Toyota", "Camry", "Sedan", "White", 5, 25000),
+       EnhancedCar("Ford", "Mustang", "Coupe", "Red", 4, 35000),
+       EnhancedCar("Chevrolet", "Tahoe", "SUV", "Black", 7, 50000),
+       EnhancedCar("Honda", "Civic", "Sedan", "Blue", 5, 20000),
+       EnhancedCar("Tesla", "Model S", "Sedan", "Silver", 5, 75000),
+       EnhancedCar("BMW", "X5", "SUV", "Gray", 5, 60000),
+       EnhancedCar("Mercedes-Benz", "C-Class", "Sedan", "Black", 5, 40000),
+       EnhancedCar("Audi", "A4", "Sedan", "White", 5, 37000),
+       EnhancedCar("Jeep", "Wrangler", "SUV", "Green", 4, 45000),
+       EnhancedCar("Porsche", "911", "Coupe", "Yellow", 4, 100000)};
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow) {
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
     ui->setupUi(this);
 
     maxPriceFilter = 0;
     carTypeFilter.clear();
     carManufacturerFilter.clear();
 
-    connect(ui->maxPriceInput, &QLineEdit::textChanged, this,
-            &MainWindow::updateMaxPriceFilter);
-    connect(ui->carTypeList, &QComboBox::currentTextChanged, this,
-            &MainWindow::updateCarTypeFilter);
-    connect(ui->carManufacturerList, &QComboBox::currentTextChanged, this,
+    connect(ui->maxPriceInput, &QLineEdit::textChanged, this, &MainWindow::updateMaxPriceFilter);
+    connect(ui->carTypeList, &QComboBox::currentTextChanged, this, &MainWindow::updateCarTypeFilter);
+    connect(ui->carManufacturerList,
+            &QComboBox::currentTextChanged,
+            this,
             &MainWindow::updateCarManufacturerFilter);
 
     setUpFilters();
 }
 
-void MainWindow::setUpFilters() {
+void MainWindow::setUpFilters()
+{
     int maxCarPrice = 0;
 
     ui->carTypeList->addItem("All");
@@ -49,8 +52,7 @@ void MainWindow::setUpFilters() {
             maxCarPrice = carPrice;
 
         QString carType = QString::fromStdString(currentCar.getType());
-        QString carManufacturer =
-            QString::fromStdString(currentCar.getManufacturer());
+        QString carManufacturer = QString::fromStdString(currentCar.getManufacturer());
 
         if (ui->carTypeList->findText(carType) == -1)
             ui->carTypeList->addItem(carType);
@@ -62,7 +64,8 @@ void MainWindow::setUpFilters() {
     ui->maxPriceInput->setText(QString::number(maxCarPrice));
 }
 
-void MainWindow::updateCarList() {
+void MainWindow::updateCarList()
+{
     ui->carList->clear();
 
     for (int index = 0; index < MAX_CARS_COUNT; index++) {
@@ -77,36 +80,39 @@ void MainWindow::updateCarList() {
         if (carTypeFilter != "All" && carType != carTypeFilter)
             continue;
 
-        if (carManufacturerFilter != "All" &&
-            carManufacturer != carManufacturerFilter)
+        if (carManufacturerFilter != "All" && carManufacturer != carManufacturerFilter)
             continue;
 
         string tempBuffer = "";
-        tempBuffer +=
-            currentCar.getManufacturer() + " " + currentCar.getModelRange() + " ";
+        tempBuffer += currentCar.getManufacturer() + " " + currentCar.getModelRange() + " ";
         tempBuffer += "[Type: " + currentCar.getType() + "] ";
         tempBuffer += "[Price: " + to_string(currentCar.getPrice()) + "] ";
         tempBuffer += "[Color: " + currentCar.getColor() + "] ";
-        tempBuffer +=
-            "[Seats Count: " + to_string(currentCar.getSeatsCount()) + "]";
+        tempBuffer += "[Seats Count: " + to_string(currentCar.getSeatsCount()) + "]";
 
         ui->carList->addItem(QString::fromStdString(tempBuffer));
     }
 }
 
-void MainWindow::updateMaxPriceFilter(const QString &price) {
+void MainWindow::updateMaxPriceFilter(const QString &price)
+{
     maxPriceFilter = price.toInt();
     updateCarList();
 }
 
-void MainWindow::updateCarTypeFilter(const QString &carType) {
+void MainWindow::updateCarTypeFilter(const QString &carType)
+{
     carTypeFilter = carType.toStdString();
     updateCarList();
 }
 
-void MainWindow::updateCarManufacturerFilter(const QString &carManufactuer) {
+void MainWindow::updateCarManufacturerFilter(const QString &carManufactuer)
+{
     carManufacturerFilter = carManufactuer.toStdString();
     updateCarList();
 }
 
-MainWindow::~MainWindow() { delete ui; }
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
